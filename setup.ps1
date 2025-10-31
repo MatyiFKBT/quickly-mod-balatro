2fc0aabb64f2d80fc7c4134b4d2feea8d1c33558
MatyiFKBT <6183867+MatyiFKBT@users.noreply.github.com>
2025-10-31 10:23:50 +0100

    feat: support new separate Steamodded examples repository (#7)

--- setup.ps1
+++ setup.ps1
@@ -111,9 +111,20 @@ if (-not (Test-Path "$modsPath\Steamodded"))
   git -C "$modsPath\Steamodded" pull
 }
 
-# copy example_mods/Mods/AchievementsEnabler.lua and MoreSpeeds.lua to %AppData%\Balatro\Mods\Steamodded\Mods
+# Clone or update the Steamodded examples repository which now contains example mods
+if (-not (Test-Path "$modsPath\Steamodded-examples"))
+{
+  Write-Output "Cloning Steamodded examples repository to $modsPath\Steamodded-examples..."
+  git clone https://github.com/Steamodded/examples.git "$modsPath\Steamodded-examples"
+} else
+{
+  Write-Output "Steamodded examples already cloned, pulling latest changes..."
+  git -C "$modsPath\Steamodded-examples" pull
+}
+
+# copy example mods (AchievementsEnabler.lua and MoreSpeeds.lua) from the separate examples repo to %AppData%\Balatro\Mods
 # if the file already exists, overwrite it
-$exampleModsPath = "$modsPath\Steamodded\example_mods\Mods"
+$exampleModsPath = "$modsPath\Steamodded-examples\Mods"
 Copy-Item "$exampleModsPath\AchievementsEnabler.lua" "$modsPath\AchievementsEnabler.lua" -Force
 Copy-Item "$exampleModsPath\MoreSpeeds.lua" "$modsPath\MoreSpeeds.lua" -Force
 
